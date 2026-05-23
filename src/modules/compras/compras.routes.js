89e933dab96905681e85d152c827d8acf8ba4156
Jose Nieves <107650316+proontheway@users.noreply.github.com>
2026-05-22 22:16:17 -0500

    Corrige rutas defensivas y vista 404 en compras

--- src/modules/compras/compras.routes.js
+++ src/modules/compras/compras.routes.js
@@ -12,17 +12,23 @@ router.use(requiereRol('administrador'));
 router.get('/cuentas-por-pagar', comprasController.mostrarCuentasPorPagar);
 router.get('/pagos-proveedores', comprasController.mostrarPagosProveedores);
 router.get('/nueva', comprasController.mostrarFormularioNuevaCompra);
+
+// Alias defensivo para evitar que /compras/formulario sea interpretado como /compras/:id
+router.get('/formulario', (req, res) => {
+    return res.redirect('/compras/nueva');
+});
+
 router.get('/api/productos/buscar', comprasController.buscarProductosParaCompra);
 router.post('/api/validar', comprasController.validarCompra);
 
 router.post('/', comprasController.guardarCompra);
 router.get('/', comprasController.listarCompras);
 
-router.get('/:id/pagos/nuevo', comprasController.mostrarFormularioPagoProveedor);
-router.post('/:id/pagos', comprasController.registrarPagoProveedor);
-router.post('/:id/pagos/:idPago/anular', comprasController.anularPagoProveedor);
+router.get('/:id(\\d+)/pagos/nuevo', comprasController.mostrarFormularioPagoProveedor);
+router.post('/:id(\\d+)/pagos', comprasController.registrarPagoProveedor);
+router.post('/:id(\\d+)/pagos/:idPago(\\d+)/anular', comprasController.anularPagoProveedor);
 
-router.get('/:id/imprimir', comprasController.mostrarImprimirCompra);
-router.get('/:id', comprasController.mostrarDetalleCompra);
+router.get('/:id(\\d+)/imprimir', comprasController.mostrarImprimirCompra);
+router.get('/:id(\\d+)', comprasController.mostrarDetalleCompra);
 
 module.exports = router;
